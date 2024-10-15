ff99e309073fb0c4be7d9c5d248c8fde0d5e2138
gcannaud <gcannaud@42VM.myguest.virtualbox.org>
2024-10-15 13:23:10 +0200

    heredoc debug

--- src/parsing/heredoc/set_heredoc.c
+++ src/parsing/heredoc/set_heredoc.c
@@ -79,6 +79,7 @@ char	*set_heredoc(t_minishell *shell, t_set_fd *set_fd, char *block, int *i)
 	if (exit_code == 1)
 		set_fd->error = 1;
 	block = rm_redirect(block, j, *i, &set_fd->error);
+	*i = j - 1;
 	if (ft_strlen(block) == 0)
 		*i = 0;
 	return (block);
